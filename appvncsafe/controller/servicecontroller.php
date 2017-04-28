bfc7226c71b3f6e588c77034ff31eaf995ac7f9b
Kapil Nadiyapara <kapil.nadiyapara@vnc.biz>
2017-04-28 04:40:18 -0400

    [30003017-741] Fix: Files/Folders do not bring favorite named %3A, %3B

--- appvncsafe/controller/servicecontroller.php
+++ appvncsafe/controller/servicecontroller.php
@@ -567,7 +567,6 @@ class ServiceController extends ApiController {
 	*	@NoAdminRequired
 	*/
 	public function setUnsetFavorites($path , $tag) {
-		$path = urldecode($path);
 		$path = str_replace('\\','/',$path);
 		$tagName = '_%24!%3CFavorite%3E!%24_';
 		$tagArray = array(urldecode($tagName));
